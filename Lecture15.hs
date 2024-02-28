ea7d994749a0505cf2359048c0796dc44baaeead
RexMortem <68436000+RexMortem@users.noreply.github.com>
2024-02-28 10:46:54 +0000

    Added README

--- Lecture15.hs
+++ Lecture15.hs
@@ -1,10 +1,16 @@
 -- Records, more on Newtype, data-orientated programming 
 
+{-  NOTE
+
+    The lecture note section starts at line 69
+    I would recommend reading the lecture note part rather than the lecture part 
+-}
+
 {-
     We can use record syntax to provide names to parameters. This can help us be clear what arguments are which.
     Having to remember order for constructing a data type can lead to MISTAKES!!!
 
-    Record syntax is syntactic sugar 
+    Record syntax is syntactic sugar for ADTs
 -}
 
 data Rectangle = Rect {
@@ -18,7 +24,7 @@ area :: Rectangle -> Int
 area aRectangle = width aRectangle * height aRectangle
 
 area' :: Rectangle' -> Int 
-area' (Rect' x y) = x * y -- for some reason x,y here can be named as the same as the width, height accessor functions  
+area' (Rect' x y) = x * y -- for some reason x,y here can be named as the same as the width, height accessor functions (local scope?) 
 
 data Square = Square {
     -- width :: Int -> Multiple declarations of "width" error
@@ -40,8 +46,8 @@ changeWidth' newWidth (Rect' width height) = Rect' newWidth height
 
 {-  CONSTRAINTS FOR NEWTYPE
 
-    - One constructor 
-    - Constructor must have one parameter
+    - One data constructor 
+    - Data Constructor must have one parameter
 -}
 
 newtype Backwards = Backwards {
@@ -79,10 +85,9 @@ me = Student "Edward" "Denton" (Just "Ed") [CS141, CS139]
 
 accessName :: Student -> String 
 accessName (Student fn ln _ _) = fn ++ " " ++ ln
--- enter records: the best syntactic sugar 
+-- enter records: the coolest syntactic sugar 
 
 data Student' = Student' {firstName :: String, lastName :: String, preferredName :: Maybe String, modules :: [Module]} deriving Show
--- even the default Show implementation is more ergonomic! 
 
 {-
     Naming the fields has three main effects:
@@ -102,6 +107,10 @@ me' = Student' { -- so ergonomic
     preferredName = Just "Ed",
     modules = [CS139, CS141]
 }
+
+-- even the default Show implementation is more readable!! ->
+-- me: Student "Edward" "Denton" (Just "Ed") [CS141,CS139]
+-- me': Student' {firstName = "Edward", lastName = "Denton", preferredName = Just "Ed", modules = [CS139,CS141]}
 
 -- Ex 1 
 
@@ -128,8 +137,7 @@ cs139 = Module' {
     department = "CS"
 }
 
--- REMEMBER ADTs are capitalised and so are their constructors!! In this sense, constructors aren't normal functions
--- ALSO you need to use a constructor!! 
+-- REMEMBER ADTs are capitalised and so are their data constructors!! In this sense, data constructors aren't normal functions
 
 cs126 :: Module' 
 cs126 = Module' {
@@ -147,7 +155,7 @@ cs133 = Module' {
     cats = 15
 }
 
--- since record syntax IS just syntactic sugar, we can use the constructor in the less ergonomic way as well
+-- since record syntax IS just syntactic sugar, we can use the data constructor in the less ergonomic way as well
 
 cs118 :: Module' 
 cs118 = Module' "118" [1] 15 "CS" -- this DOES need to be in order though 
@@ -179,6 +187,113 @@ data Animal = Dog {
 data Person = Person {
     personName :: String, -- if we put this as animalName then it would be a multiple declarations error!! 
     personAge :: Int 
+} deriving Show 
+
+{-  UPDATING RECORDS
+
+    Previously, we had to deconstruct and reconstruct ADTs to change even a single field
+    Now, we can use record syntax which is much easier for large ADTs (imagine updating an ADT with 5+ fields)
+-}
+
+updateAge :: Int -> Person -> Person 
+updateAge newAge (Person name _) = Person name newAge -- old way 
+
+updateAge' :: Int -> Person -> Person 
+updateAge' newAge ourADT = ourADT {personAge = newAge} -- don't have to reconstruct!!!
+{-
+    In GHCI:
+    a = Person "Ed" 18
+    b = a {personAge = 19}
+-}
+
+changeDepartment :: String -> Module' -> Module' 
+changeDepartment newDept aModule = aModule {department = newDept}
+
+updateYears :: Module' -> Module' 
+updateYears aModule = let 
+                          yearToAdd = read [(head.code) aModule] :: Int
+                          newYear :: [Int]
+                          newYear = if yearToAdd `elem` year aModule then year aModule else yearToAdd : year aModule 
+                      in aModule {year = newYear}
+
+-- probably best to use a where clause for yearToAdd combined with a guard clause (or a case of)
+
+{-  NEWTYPE
+
+    Used for wrapping types mainly (adding extra or different functionality to existing types, or just to distinguish them)
+    Newtype is for special cases of ADTs: single data constructor with a single parameter
+
+    For example, I have written the binary ADT whose main purpose is to change Show so that its string representation
+    is the binary version of the number. 
+    
+    Due to type erasure, there is no extra cost incurred by using Cm or Mm as opposed to using Integers. 
+-}
+
+newtype Cm = ToCm {
+    fromCm :: Integer 
 }
 
--- Updating data 
+newtype Mm = ToMm {
+    fromMm :: Integer 
+}
+
+newtype Binary = ToBinary {
+    fromBinary :: Integer 
+}
+
+buildBinaryString :: Binary -> String 
+buildBinaryString bn
+    | n == 0 = ""
+    | otherwise = bit : buildBinaryString binaryResult 
+    where 
+          n :: Integer 
+          n = fromBinary bn 
+
+          (result, remainder) = divMod n 2 
+          
+          bit :: Char 
+          bit = if remainder == 0 then '0' else '1' 
+
+          binaryResult :: Binary 
+          binaryResult = ToBinary result
+
+instance Show Binary where 
+    show :: Binary -> String 
+    show = reverse.buildBinaryString
+
+{- 
+    To make it easier to think about kinds next lecture, know that Binary is a TYPE 
+
+    We create a value of type Binary using Binary's data constructors 
+    In this case, as it is a newtype, there is only one data constructor: ToBinary 
+
+    So in:
+
+    newtype Binary = ToBinary Integer 
+
+    left-side is a type
+    right-side has data constructor 
+-}
+
+data LinkedList a = EmptyList | Singleton a | Prepend a (LinkedList a) deriving Show
+
+{-
+    For this, we also have a type variable. LinkedList is no longer just a type because we can have several LinkedLists of different types.
+    For instance, LinkedList String is not the same type as LinkedList Integer.
+
+    Similarly, it does not make sense to have a list of type LinkedList. 
+    It needs to be a list containing *something* whether it's a String or Bool or Int. 
+
+    So what is LinkedList? It's a type constructor. 
+    It takes in a type variable (a) and produces a new type (LinkedList a)! 
+    It still has data constructors and these create values of the type specified by the type constructor. 
+
+    We know that each of the data constructors are basically functions and therefore have types:
+
+    EmptyList :: LinkedList a 
+    Singleton :: a -> LinkedList a 
+    Prepend :: a -> LinkedList a -> LinkedList a
+
+    But the parameters of the type constructor LinkedList are types themselves. So what is the type of the type constructor?
+    That is the subject of the next lecture: kinds. 
+-}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Structure of Lecture files
 
 Sometimes Lecture files are split into notes/experimentation I took/did during the lecture, and notes I took from the lecture notes. 
+
+# Repeated Content 
+
+Since these lecture files are made for my own understanding, there may be repetition of concepts as I may write from different perspectives or to refresh myself. As such, there may also be incorrect information within these lecture files as I am learning the content. 
+
+# Reference material 
+
+All of the material that the lecture files are based off is in the functional programming module (CS141) at the University of Warwick. 
+These materials are amazing and you probably have access to them if, for some reason, you're reading this. 
